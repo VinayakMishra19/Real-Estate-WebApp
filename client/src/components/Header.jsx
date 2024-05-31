0efc58e31118b9e10f574094f1748ee4b2466c95
vinayakmishra19 <vinayakmishra1978@gmail.com>
2024-05-31 22:31:43 +0530

    Created Header Component and Link-routing

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,31 @@
+import {FaSearch} from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <header className='bg-slate-300 shadow-md'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+          
+          <Link to='/'>
+          <h1 className='font-bold text-sm sm:text-xl flex flex-wrap select-none hover:cursor-pointer'>
+            <span className='text-slate-500 '>V-Real</span>
+            <span className='text-slate-700'>Estate</span>
+        </h1>
+        </Link>
+
+        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+            <input type='text' placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
+            <FaSearch className='text-slate-600 hover:cursor-pointer'/>
+        </form> 
+
+        <ul className='flex gap-4'>
+            <Link to='/'> <li className='hidden sm:inline hover:underline hover:cursor-pointer'>Home</li></Link>
+            <Link to='/about'><li className='hidden sm:inline hover:underline hover:cursor-pointer'>About</li></Link>
+            <Link to='/sign-in'><li className='hover:underline hover:cursor-pointer'>Sign-In</li></Link>
+         </ul> 
+         
+        </div>
+        
+    </header>
+  )
+}
