22da5fcd32dfd78c834fcb663b362565ea730fe1
vinayakmishra19 <vinayakmishra1978@gmail.com>
2024-06-03 01:16:14 +0530

    created listing api route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,4 +1,3 @@
-import { timeStamp } from "console";
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
@@ -20,7 +19,7 @@ const userSchema = new mongoose.Schema({
         type:String,
         default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
     },
-},{timeStamp: true});
+},{timestamps: true});
 
 const User = mongoose.model('User', userSchema);
 
