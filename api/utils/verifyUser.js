f3d621674df742caa8218c418c242f18a6294016
vinayakmishra19 <vinayakmishra1978@gmail.com>
2024-06-02 17:08:58 +0530

    complete update user functionality

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import { errorHandler } from "./error.js";
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
@@ -6,7 +7,7 @@ export const verifyToken = (req, res, next) => {
   if (!token) return next(errorHandler(401, "Unauthorized"));
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return next(errorHandler(403, "Forbidden"));
+    if (err) return next(errorHandlerndler(403, "Forbidden"));
 
     req.user = user;
     next();
