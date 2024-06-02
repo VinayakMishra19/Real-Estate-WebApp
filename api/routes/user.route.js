ac46280dedbc42e89e3dbd03c4cb49ade7fb6af7
vinayakmishra19 <vinayakmishra1978@gmail.com>
2024-06-03 00:03:14 +0530

    add sign out functionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -2,9 +2,11 @@ import express from 'express';
 import { deleteUser, test, updatedUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
+
 const router = express.Router();
 
 router.get('/test', test);
 router.post('/update/:id', verifyToken, updatedUser)
 router.delete('/delete/:id', verifyToken, deleteUser)
+
 export default router;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -14,6 +14,9 @@ import {
   deleteUserFailure,
   deleteUserStart,
   deleteUserSuccess,
+  signOutStart,
+  signOutFailure,
+  signOutSuccess,
 } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 
@@ -105,6 +108,21 @@ export default function Profile() {
     } catch (error) {
       dispatch(deleteUserFailure(error.message));
     }
+  };
+
+  const handleSignOut = async () =>{
+    try {
+      dispatch(signOutStart());
+      const res = await fetch('/api/auth/signout');
+      const data = await res.json();
+      if(data.success === false){
+        dispatch(signOutFailure(data.message));
+        return;
+      }
+        dispatch(signOutSuccess(data));
+    } catch (error) {
+      dispatch(signOutFailure(error.message));
+    }
   }
   return (
     <div className="p-3 max-w-lg mx-auto">
@@ -171,7 +189,7 @@ export default function Profile() {
 
       <div className="flex justify-between mt-5">
         <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete Account</span>
-        <span className="text-red-700 cursor-pointer">Sign Out</span>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">Sign Out</span>
       </div>
 
       <p className="text-red-700 mt-5">{error ? error : ""}</p>
