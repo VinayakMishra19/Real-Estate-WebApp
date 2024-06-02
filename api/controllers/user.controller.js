ffcd4dc00cc6ceb4306938edcdafec81f2a8c7d8
vinayakmishra19 <vinayakmishra1978@gmail.com>
2024-06-02 21:54:02 +0530

    add delete user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
+
 export const test = (req, res) => {
   res.send("Api Route is Working");
 };
@@ -28,3 +29,15 @@ export const updatedUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const deleteUser = async (req, res, next) =>{
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "You can only delete ur own account"));
+  try {
+      await User.findByIdAndDelete(req.params.id);
+      res.clearCookie('access_token');
+      res.status(200).json('User has been deleted ')
+    }  catch (error) {
+      next(error);
+    }
+}
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { test, updatedUser } from '../controllers/user.controller.js';
+import { deleteUser, test, updatedUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.get('/test', test);
 router.post('/update/:id', verifyToken, updatedUser)
-
+router.delete('/delete/:id', verifyToken, deleteUser)
 export default router;
